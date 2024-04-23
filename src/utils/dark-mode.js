6205f63c4b9eebdc4b5ec317359863578272806e
Phyo-Thu-Nyein <phyothunyein14@gmail.com>
2024-04-24 00:23:47 +0530

    light/dark icons change according to the theme

--- src/utils/dark-mode.js
+++ src/utils/dark-mode.js
@@ -1,27 +1,50 @@
 const darkMode = () => {
     const themeToggleBtns = document.querySelectorAll('#theme-toggle');
+    const dayIcons = document.querySelectorAll('.day');
+    const nightIcons = document.querySelectorAll('.night');
 
     // State
     const theme = localStorage.getItem('theme');
+    const icon = localStorage.getItem('icon');
+    let isLightMode = false;
 
     // On mount
     theme && document.body.classList.add(theme);
+    const svgs = document.querySelectorAll('#night');
+    icon && svgs.forEach(one => {
+        one.classList.remove(icon);
+        dayIcons.forEach(day => {
+            day.style.display = 'none';
+            isLightMode = true;
+        })
+    });
 
     // Handlers
-    const handleThemeToglle = () => {
+    const handleThemeToggle = () => {
         document.body.classList.toggle('light-mode');
         if (document.body.classList.contains('light-mode')) {
             localStorage.setItem('theme', 'light-mode');
-
+            // document.querySelector('.night').style.display = 'block';
+            localStorage.setItem('icon', 'night');
         } else {
             localStorage.removeItem('theme');
             document.body.removeAttribute('class');
+            localStorage.removeItem('icon');
+            document.querySelector('#night').removeAttribute('.night');
+        }
+        isLightMode = !isLightMode;
+        if (isLightMode) {
+            dayIcons.forEach(one => one.style.display = 'none');
+            nightIcons.forEach(night => night.style.display = 'block');
+        } else {
+            dayIcons.forEach(one => one.style.display = 'block');
+            nightIcons.forEach(night => night.style.display = 'none');
         }
     }
 
     // Events
     themeToggleBtns.forEach(btn =>
-        btn.addEventListener('click', handleThemeToglle)
+        btn.addEventListener('click', handleThemeToggle)
     );
 
 };
